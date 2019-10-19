0847f3ae7096cf9109833d6c88c72f2788e6a547
Daniel <danielmarchan79@gmail.com>
2019-10-19 09:40:10 -0400

    Updates

---
+++ precache-manifest.c0a300d40e6c8507ee3e389a69b9681b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f4dfe4d20e7c2cd2b48",
+    "url": "/Postbook/static/js/runtime~main.5f4dfe4d.js"
+  },
+  {
+    "revision": "81b3e4f41884459996b7",
+    "url": "/Postbook/static/js/main.81b3e4f4.chunk.js"
+  },
+  {
+    "revision": "1d78a2a581f3a395e1b6",
+    "url": "/Postbook/static/js/1.1d78a2a5.chunk.js"
+  },
+  {
+    "revision": "81b3e4f41884459996b7",
+    "url": "/Postbook/static/css/main.3b8950a1.chunk.css"
+  },
+  {
+    "revision": "8a03dc7f1dd29c09458814569624bf4b",
+    "url": "/Postbook/index.html"
+  }
+];
